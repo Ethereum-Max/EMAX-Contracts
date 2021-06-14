36cc3e509f0a64cb123db3e6ca95a76be0360040
Stake Sinatra <stakesinatra@gmail.com>
2021-06-13 21:51:46 -0400

    v2 logic started

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,17 @@
 const EMaxCoin = artifacts.require('EMaxCoin');
-
+const EMaxCoinV2 = artifacts.require('EMaxCoinV2');
  
-const { deployProxy } = require('@openzeppelin/truffle-upgrades');
+const { prepareUpgrade, deployProxy } = require('@openzeppelin/truffle-upgrades');
  
 module.exports = async function (deployer) {
-  await deployProxy(EMaxCoin, [], { deployer, initializer: 'initialize' });
-  
+  // const eMaxV1 = await EMaxCoin;
+  // if(eMaxV1){
+    await deployProxy(EMaxCoinV2, [], { deployer, initializer: 'initialize' });
+    const v1Deployed = await EMaxCoinV2.deployed();
+    // console.log(v1Deployed);
+  // }else{
+    // await prepareUpgrade(EMaxCoin.address, EMaxCoinV2, { deployer});
+    // const v2Deployed = await EMaxCoinV2.deployed();
+    // console.log(v2Deployed);
+  // }
 };
