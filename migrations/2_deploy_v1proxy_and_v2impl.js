a8710f768ca6b10834bd2e26965f9582214b1726
defi-devil <devildefi@gmail.com>
2021-06-16 21:20:17 -0400

    renamed migration scripts; added implementation for upgrading proxy hopefully

--- migrations/2_deploy_v1proxy_and_v2impl.js
+++ migrations/2_deploy_v1proxy_and_v2impl.js
@@ -1,8 +1,14 @@
 const EMaxCoin = artifacts.require('EMaxCoin'); 
+const EMaxCoinV2 = artifacts.require('EMaxCoinV2'); 
 const { admin, prepareUpgrade, deployProxy, admin } = require('@openzeppelin/truffle-upgrades');
  
 module.exports = async function (deployer) {
     await deployProxy(EMaxCoin, [], { deployer, initializer: 'initialize' });
-    const deployed = await EMaxCoin.deployed();
+    deployer.deploy(EMaxCoinV2.sol);
+    deployed = await EMaxCoin.deployed();
+    deployed = await EMaxCoinV2.deployed();
     admin.transferProxyAdminOwnership("0xe0449fAd286FC646a26Ef335C56efF8E4B89ce8F");
 };
+
+
+
--- migrations/3_upgrade_v1impl_to_v2impl.js
+++ migrations/3_upgrade_v1impl_to_v2impl.js
@@ -1,16 +1,13 @@
+
 const EMaxCoin = artifacts.require('EMaxCoin');
 const EMaxCoinV2 = artifacts.require('EMaxCoinV2New');
- 
+    
 const { prepareUpgrade, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
- 
+    
 module.exports = async function (deployer) {
+    const admin = admin.getInstance();
     const EMaxV2Address = await prepareUpgrade(EMaxCoin, EMaxCoinV2, { deployer });
-    const upgraded = upgradeProxy(upgradeProxy, EmaxV2, {deployer});
-    // let EMaxV2 = await EMaxCoinV2.at(EMaxV2Address);
-    // await EMaxV2.setBurnRate.call(3);
-    // await EMaxV2.setReflectRate.call(3);
-    // await EMaxV2.excludeAccount.call(account(0));
-    // await EMaxV2.excludeAccount.call('0xe0449fAd286FC646a26Ef335C56efF8E4B89ce8F');
+    const upgraded = upgradeProxy(admin.address, EmaxV2, {deployer});
 };
 
 
@@ -20,3 +17,9 @@ module.exports = async function (deployer) {
 // 3. Do a truffle veriofy on V1 using truffle verify EMax Coin
 // 4. New Deployment only Migrations file 3 - `deploy_upgrade.js`
 // 5. deploy_upgrade needs to call `upgradeProxy` function with param
+
+
+
+// first time run deploy, run lines 1 through 9; transfes ownship
+// ensure we own proxdy
+// second time, call upgrade proxy, passing in v1 and v2 as args 1 and 2
