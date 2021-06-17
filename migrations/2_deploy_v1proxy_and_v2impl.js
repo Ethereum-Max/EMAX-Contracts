a949f16e21eed6665998ff1a945ae73f75c73344
defi-devil <devildefi@gmail.com>
2021-06-16 21:38:46 -0400

    updated migration2 script

--- migrations/2_deploy_v1proxy_and_v2impl.js
+++ migrations/2_deploy_v1proxy_and_v2impl.js
@@ -1,12 +1,12 @@
 const EMaxCoin = artifacts.require('MaxEthereum'); 
 const EMaxCoinV2 = artifacts.require('MaxEthereumV2'); 
-const { admin, prepareUpgrade, deployProxy, admin } = require('@openzeppelin/truffle-upgrades');
+const { deployProxy, admin } = require('@openzeppelin/truffle-upgrades');
  
 module.exports = async function (deployer) {
     await deployProxy(EMaxCoin, [], { deployer, initializer: 'initialize' });
     deployer.deploy(EMaxCoinV2.sol);
-    deployed = await EMaxCoin.deployed();
-    deployed = await EMaxCoinV2.deployed();
+    const deployed1 = await EMaxCoin.deployed();
+    const deployed2 = await EMaxCoinV2.deployed();
     admin.transferProxyAdminOwnership("0xe0449fAd286FC646a26Ef335C56efF8E4B89ce8F");
 };
 
