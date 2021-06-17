4510f787417b2d03422c0bae6ca491211af66ca4
defi-devil <devildefi@gmail.com>
2021-06-16 20:57:41 -0400

    added comments

--- migrations/3_deploy_upgrade.js
+++ migrations/3_deploy_upgrade.js
@@ -12,3 +12,11 @@ module.exports = async function (deployer) {
     // await EMaxV2.excludeAccount.call(account(0));
     // await EMaxV2.excludeAccount.call('0xe0449fAd286FC646a26Ef335C56efF8E4B89ce8F');
 };
+
+
+
+// 1. reset truffle migrations to remove previous deployment data
+// 2. do first deployment by deploying V1 also with proxy
+// 3. Do a truffle veriofy on V1 using truffle verify EMax Coin
+// 4. New Deployment only Migrations file 3 - `deploy_upgrade.js`
+// 5. deploy_upgrade needs to call `upgradeProxy` function with param
