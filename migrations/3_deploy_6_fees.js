6e9d0a4ad3a29f8655572d11e98ca93440c8cbef
emaxelicious <Emaxelicious@protonmail.com>
2021-06-24 18:20:26 -0400

    update mainnet.json from release

--- migrations/3_deploy_6_fees.js
+++ migrations/3_deploy_6_fees.js
@@ -4,7 +4,8 @@ const EMaxCoin2 = artifacts.require('EMaxCoin2');
 const { upgradeProxy } = require('@openzeppelin/truffle-upgrades');
 
 module.exports = async function (deployer) {
-  const deployed = await EMaxCoin.deployed();
-  await upgradeProxy(deployed.address, EMaxCoin2, {deployer});
-
+  // const deployed = await EMaxCoin.deployed();
+  // await upgradeProxy(deployed.address, EMaxCoin2, {deployer});
+  
+  await upgradeProxy("0x15874d65e649880c2614e7a480cb7c9A55787FF6", EMaxCoin2, {deployer});
 };
